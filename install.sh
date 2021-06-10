d133ea1a27c8aeb4a9aa514a27b16173c0db5a3b
amei <cms1984@msn.com>
2021-06-10 16:11:21 +0800

    sh安装

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ if [ ! -d "~/.config/nvim/autoload" ]; then
   mkdir -p ~/.config/nvim/autoload
 fi
 
-if [ ! -d "./vim-plug" ]; then
-	echo '[*] Downloading vim-plug ...'
+if ! [ -e ~/.config/nvim/autoload/plug.vim ]; then
+	echo "[*] Installing vim-plug"
 	#curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 		#https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	git clone https://github.com/junegunn/vim-plug
@@ -22,9 +22,11 @@ if [ ! -d "./vim-plug" ]; then
 fi
 
 # install config
-#ln -sf `pwd`/vim/vimrc ~/.config/nvim/init.vim
+ln -sf `pwd`/vim/vimrc ~/.config/nvim/init.vim
 
 echo -e '[*] Running :PlugInstall within nvim ...'
 nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall'
+
+CocInstall coc-phpls
 
 echo -e "[+] Done"
