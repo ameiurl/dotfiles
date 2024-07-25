243763be2fad8caa929d57965e2365854674223f
amei <cms1984@msn.com>
2024-07-26 05:54:05 +0800

    neo-tree

--- nvim/lua/config/neo-tree.lua
+++ nvim/lua/config/neo-tree.lua
@@ -125,6 +125,46 @@ require("neo-tree").setup({
                 cwd = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h"),
             })
         end,
+        copy_selector = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local vals = {
+                ["BASENAME"] = modify(filename, ":r"),
+                ["EXTENSION"] = modify(filename, ":e"),
+                ["FILENAME"] = filename,
+                ["PATH (CWD)"] = modify(filepath, ":."),
+                ["PATH (HOME)"] = modify(filepath, ":~"),
+                ["PATH"] = filepath,
+                ["URI"] = vim.uri_from_fname(filepath),
+            }
+
+            local options = vim.tbl_filter(
+                function(val)
+                    return vals[val] ~= ""
+                end,
+                vim.tbl_keys(vals)
+            )
+            if vim.tbl_isempty(options) then
+                vim.notify("No values to copy", vim.log.levels.WARN)
+                return
+            end
+            table.sort(options)
+            vim.ui.select(options, {
+                prompt = "Choose to copy to clipboard:",
+                format_item = function(item)
+                    return ("%s: %s"):format(item, vals[item])
+                end,
+            }, function(choice)
+                    local result = vals[choice]
+                    if result then
+                        vim.notify(("Copied: `%s`"):format(result))
+                        vim.fn.setreg("+", result)
+                    end
+                end)
+        end,
     },
 	window = {
 		position = "left",
@@ -189,6 +229,7 @@ require("neo-tree").setup({
 			["<"] = "prev_source",
 			[">"] = "next_source",
 			["i"] = "show_file_details",
+            ["Y"] = "copy_selector",
 		},
 	},
 	nesting_rules = {},
