fd8d7443e6c070a770463c389d1c9e86e458d055
amei <cms1984@msn.com>
2025-04-17 08:00:31 +0800

    开启终端，自动启动tmux

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,3 +4,10 @@ export XDG_CONFIG_HOME=$HOME/.config
 export EDITOR=nvim
 export SUDO_EDITOR=/usr/local/bin/nvim
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7891
+
+# if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+    # tmux attach -t main 2>/dev/null || (tmux new-session -s main && exit)
+# fi
+if [ -z "$TMUX" ]; then
+  exec tmux new-session -A -s main
+fi
