832588176ae717868e05dfc1accedb64bc44f6b2
amei <cms1984@msn.com>
2025-05-02 14:52:21 +0800

    update .gitignore

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,7 +4,3 @@ export XDG_CONFIG_HOME=$HOME/.config
 export EDITOR=nvim
 export SUDO_EDITOR=/usr/local/bin/nvim
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7891
-export OPENAI_API_KEY=sk-1b3fd15b56ed47e6afe261b7c2632812
-export DEEPSEEK_API_KEY=sk-1b3fd15b56ed47e6afe261b7c2632812
-export DEEPSEEK_API_KEY_S=sk-wgivzzqfmysojvvpbvgdammqfprahxpwjlfmzxutxmsnpkhj
-export DEEPSEEK_API_ALIYUN=c27eff6c29b3490fa58605acbd353324
