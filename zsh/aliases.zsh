a35a41954321c5d70cd2f8c7e04591bc1f3174ef
amei <cms1984@msn.com>
2025-11-18 16:14:01 +0800

    alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,3 +13,14 @@ alias ga='git add'
 alias gcmsg='git commit -m'
 alias ggpush='git push'
 alias ggpull='git pull'
+
+alias cc="claude --dangerously-skip-permissions" 
+alias cc-glm="ANTHROPIC_AUTH_TOKEN=724ece6cdc05461ab0572959371a47da.zGVbVtZrCJvEK2Ti \
+ANTHROPIC_BASE_URL=https://open.bigmodel.cn/api/anthropic \
+ANTHROPIC_SMALL_FAST_MODEL=GLM-4.6 \
+ANTHROPIC_MODEL=GLM-4.6 \
+claude --dangerously-skip-permissions"
+alias cc-kimi="ANTHROPIC_AUTH_TOKEN=your-token \
+ANTHROPIC_BASE_URL=https://api.moonshot.cn/anthropic \
+ANTHROPIC_MODEL=kimi-k2-0905 \
+claude --dangerously-skip-permissions"
