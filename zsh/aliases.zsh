08d92a47e951d5e3bfe003e59b794eeb985224d0
chen <cms1984@msn.com>
2026-08-28 20:41:54 +0800

    ds

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,3 +16,4 @@ alias ggpull='git pull'
 
 alias claude-code='~/claude-code/dist/cli.js --dangerously-skip-permissions'
 alias claude="claude --dangerously-skip-permissions" 
+alias ds='dsh --profile tui'
